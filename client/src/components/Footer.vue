d4736fb5b9316ea340d213d97d3bb17bb3aa4152
JamesGardner1 <jamesacgardner@yahoo.com>
2019-05-10 17:24:20 -0500

    Progress

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer">
+        <p>Todo - links to different parts of site</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footer'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header">
+
+    <div class="jumbotron">
+        <h1 class="display-4" id="endgame">Are You Ready For The Endgame?</h1>
+    </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+    .jumbotron {
+        background-image: url('../assets/avengersready.jpg');
+        background-position: center;
+        background-size: cover;
+        background-blend-mode: luminosity;
+        ;
+
+    }
+    #endgame {
+        font-weight: 600;
+        color:darkgoldenrod;
+    }
+
+</style>
